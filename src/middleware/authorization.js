d8eef4679b5fcf51060fe5eb13bdaa52ee90d1df
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-10 20:24:59 -0400

    created auth to work and did error testing

--- src/middleware/authorization.js
+++ src/middleware/authorization.js
@@ -1,8 +1,58 @@
 function requireAuth(req, res, next) {    
     
-    console.log(`Entered authorization`);
-    console.log(req.get('Authorization'));
-    next();
+    // get the authToken from the req
+    const authToken = req.get('Authorization');
+    let token = '';
+
+    if(!authToken){
+        return res  
+            .status(401)
+            .json({
+                error: `Unauthorized Access`
+            });
+    }else{
+        token = authToken.slice(`basic `.length, authToken.length);
+    }
+    
+    // User Buffer.from to get the username and password
+    const [tokenUsername, tokenPassword] = Buffer
+        .from(token, 'base64')
+        .toString()
+        .split(':');
+
+    
+    // check if it exists , if not then throw an error
+    // if it does, splice the basic and get the token
+    if(!tokenUsername || !tokenPassword){
+        return res
+            .status(400)
+            .json({
+                error: `Bad request`
+            })
+    }
+
+    // knexInstance for the database connection
+    const db = req.app.get('db');
+    return db('thingful_users')
+        .where({ user_name: tokenUsername })
+        .first()
+        .then((user) => {
+            if(!user){
+                return res
+                    .status(400)
+                    .json({
+                        error: `User does not exist`
+                    });
+            }
+
+            req.user = user;
+            next();
+        })
+    
+
+    
+    // if it does exists then matching password using the hash
+    // if everything is good then req.user = user, next()
 
 }
 
