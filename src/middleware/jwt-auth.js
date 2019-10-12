db4fa9c550477346ee2d171a3a043d6b602b0219
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-12 13:25:04 -0400

    created the jwt-auth.js

--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -1,6 +1,54 @@
+const AuthService = require('../auth/auth-service');
+
 function requireAuth(req, res, next){
 
-    
+    const authToken = req.get('Authorization');
+
+    let bearerToken = '';
+
+    if(!authToken.toLowerCase().startsWith('bearer ')){
+        return res
+            .status(401)
+            .json({
+                error: `Missing bearer token`
+            })
+    }else{
+        bearerToken = authToken.slice(`bearer `.length, authToken.length);
+    }
+
+    try{
+
+        const payload = AuthService.verifyJwt(bearerToken)
+        
+        AuthService.getUserWithUserName(
+            req.app.get('db'),
+            payload.sub
+        )
+            .then((user) => {
+                if(!user){
+                    return res
+                        .status(400)
+                        .json({
+                            error: `Bad Request`
+                        })
+                }
+
+                req.user = user
+                next();
+            })
+            .catch((error) => {
+                console.log(error)
+                next(error)
+            })
+
+    }catch(error){
+        res
+            .status(401)
+            .json({
+                error: `Unauthorized Request`
+            })
+
+    }
 
 }
 
